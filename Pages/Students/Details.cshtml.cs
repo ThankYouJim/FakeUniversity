8971b56f05020f5b4471a8fcc605e89881e0d8b6
Jo <jo3cyc@gmail.com>
2019-10-30 12:56:20 +1100

    Add project files.

---
+++ Pages/Students/Details.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FakeUniversity.Data;
+using FakeUniversity.Models;
+
+namespace FakeUniversity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly FakeUniversity.Data.SchoolContext _context;
+
+        public DetailsModel(FakeUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+            Student = await _context.Students
+                .Include(s => s.Enrollments)
+                .ThenInclude(e => e.Count)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
